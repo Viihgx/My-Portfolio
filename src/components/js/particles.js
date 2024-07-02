42e7ca70713f5e23c265f42d9f4b56bf20b01f2d
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-02 18:16:40 -0300

    :sparkles: Adiciona responsividade para telas medias e pequenas para as Particles

--- src/components/js/particles.js
+++ src/components/js/particles.js
@@ -1,93 +1,104 @@
 class Particle {
-    constructor(x, y, radius, color, velocity) {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-      this.velocity = velocity;
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.fillStyle = this.color;
+    context.fill();
+    context.closePath();
+  }
+
+  update(context, width, height) {
+    if (this.x - this.radius < 0 || this.x + this.radius > width) {
+      this.velocity.x = -this.velocity.x;
     }
-  
-    draw(context) {
-      context.beginPath();
-      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      context.fillStyle = this.color;
-      context.fill();
-      context.closePath();
+
+    if (this.y - this.radius < 0 || this.y + this.radius > height) {
+      this.velocity.y = -this.velocity.y;
     }
-  
-    update(context, width, height) {
-      if (this.x - this.radius < 0 || this.x + this.radius > width) {
-        this.velocity.x = -this.velocity.x;
+
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    this.draw(context);
+  }
+}
+
+function connectParticles(particles, context) {
+  let opacityValue = 1;
+  for (let a = 0; a < particles.length; a++) {
+    for (let b = a; b < particles.length; b++) {
+      let distance = Math.sqrt(
+        (particles[a].x - particles[b].x) ** 2 + (particles[a].y - particles[b].y) ** 2
+      );
+
+      if (distance < 100) {
+        opacityValue = 1 - distance / 100;
+        context.strokeStyle = `rgba(255, 255, 255, ${opacityValue})`;
+        context.lineWidth = 1;
+        context.beginPath();
+        context.moveTo(particles[a].x, particles[a].y);
+        context.lineTo(particles[b].x, particles[b].y);
+        context.stroke();
+        context.closePath();
       }
-  
-      if (this.y - this.radius < 0 || this.y + this.radius > height) {
-        this.velocity.y = -this.velocity.y;
-      }
-  
-      this.x += this.velocity.x;
-      this.y += this.velocity.y;
-  
-      this.draw(context);
     }
   }
+}
+
+export default function initParticles(canvasId) {
+  const canvas = document.getElementById(canvasId);
+  const context = canvas.getContext('2d');
+
+  canvas.width = canvas.offsetWidth;
+  canvas.height = canvas.offsetHeight;
+
+  const particles = [];
+  const colors = ['rgba(255, 255, 255, 0.5)', 'rgba(255, 255, 255, 0.8)'];
+
+  let numberOfParticles = 100;
+  let maxRadius = 3;
   
-  function connectParticles(particles, context) {
-    let opacityValue = 1;
-    for (let a = 0; a < particles.length; a++) {
-      for (let b = a; b < particles.length; b++) {
-        let distance = Math.sqrt(
-          (particles[a].x - particles[b].x) ** 2 + (particles[a].y - particles[b].y) ** 2
-        );
-  
-        if (distance < 100) {
-          opacityValue = 1 - distance / 100;
-          context.strokeStyle = `rgba(255, 255, 255, ${opacityValue})`;
-          context.lineWidth = 1;
-          context.beginPath();
-          context.moveTo(particles[a].x, particles[a].y);
-          context.lineTo(particles[b].x, particles[b].y);
-          context.stroke();
-          context.closePath();
-        }
-      }
-    }
+  // Ajustar o número de partículas e o tamanho do raio conforme a largura da tela
+  if (window.innerWidth <= 768) {
+    numberOfParticles = 50;
+    maxRadius = 2;
+  } else if (window.innerWidth <= 1024) {
+    numberOfParticles = 75;
+    maxRadius = 2.5;
   }
-  
-  export default function initParticles(canvasId) {
-    const canvas = document.getElementById(canvasId);
-    const context = canvas.getContext('2d');
-  
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-  
-    const particles = [];
-    const colors = ['rgba(255, 255, 255, 0.5)', 'rgba(255, 255, 255, 0.8)'];
-  
-    for (let i = 0; i < 100; i++) {
-      const radius = Math.random() * 3 + 1;
-      const x = Math.random() * (canvas.width - radius * 2) + radius;
-      const y = Math.random() * (canvas.height - radius * 2) + radius;
-      const color = colors[Math.floor(Math.random() * colors.length)];
-      const velocity = {
-        x: (Math.random() - 0.5) * 1,
-        y: (Math.random() - 0.5) * 1,
-      };
-  
-      particles.push(new Particle(x, y, radius, color, velocity));
-    }
-  
-    function animate() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-  
-      particles.forEach(particle => {
-        particle.update(context, canvas.width, canvas.height);
-      });
-  
-      connectParticles(particles, context);
-  
-      requestAnimationFrame(animate);
-    }
-  
-    animate();
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    const radius = Math.random() * maxRadius + 1;
+    const x = Math.random() * (canvas.width - radius * 2) + radius;
+    const y = Math.random() * (canvas.height - radius * 2) + radius;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const velocity = {
+      x: (Math.random() - 0.5) * 1,
+      y: (Math.random() - 0.5) * 1,
+    };
+
+    particles.push(new Particle(x, y, radius, color, velocity));
   }
-  
+
+  function animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    particles.forEach(particle => {
+      particle.update(context, canvas.width, canvas.height);
+    });
+
+    connectParticles(particles, context);
+
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+}
