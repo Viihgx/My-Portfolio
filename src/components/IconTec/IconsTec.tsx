d58bba4772ff9710b9b5e460579874fe49313c62
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-12 14:29:18 -0300

    :sparkles: altera texto about, ajusta iconstec, adiciona button

--- src/components/IconTec/IconsTec.tsx
+++ src/components/IconTec/IconsTec.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import './IconsTec.css';
-import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare, FaNodeJs, FaPython, FaGit, FaDocker } from 'react-icons/fa';
+import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare, FaBootstrap, FaSass, FaPhp, FaNodeJs, FaDatabase, FaFigma, FaWordpress, FaGit, FaDocker } from 'react-icons/fa';
 
 const techIcons = [
   { icon: <FaHtml5 />, label: "HTML5" },
   { icon: <FaCss3Alt />, label: "CSS3" },
   { icon: <FaReact />, label: "React" },
   { icon: <FaJsSquare />, label: "JavaScript" },
+  { icon: <FaBootstrap />, label: "Bootstrap" },
+  { icon: <FaSass />, label: "Sass" },
+  { icon: <FaPhp />, label: "PHP" },
   { icon: <FaNodeJs />, label: "Node.js" },
-  { icon: <FaPython />, label: "Python" },
+  { icon: <FaDatabase />, label: "MySQL" },
+  { icon: <FaFigma />, label: "Figma" },
+  { icon: <FaWordpress />, label: "WordPress" },
   { icon: <FaGit />, label: "Git" },
-  { icon: <FaDocker />, label: "Docker" },
-  { icon: <FaNodeJs />, label: "Node.js" },
-  { icon: <FaPython />, label: "Python" },
-  { icon: <FaGit />, label: "Git" },
-  { icon: <FaDocker />, label: "Docker" },
 ];
 
 const IconsTec = () => {
--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -2,29 +2,31 @@ import React from 'react';
 import './About.css';
 import Carousel from '../../../../components/Carousel/Carousel';
 import IconsTec from '../../../../components/IconTec/IconsTec';
-// import Button from '../../../../components/Button/Button';
+import CustomButton from '../../../../components/CustomButton/CustomButton';
 
 const About = () => {
   return (
     <div className='about-container'>
-            <Carousel />
         <div className="about-section" id="about">
             <div className="about-content">
                 <h1 className="about-title">Sobre Mim</h1>
                 <p className="about-text">
-                Sou desenvolvedora frontend com experiência nas tecnologias como HTML, CSS, JavaScript, Typescript, React, Material-UI (MUI), Bootstrap, MySQL, PHP e Node/Express. Ao longo da minha jornada acadêmica, trabalhei em diversos projetos web, desde a criação de páginas até a integração a partir do Figma. Atualmente, estou no último período da faculdade, e estou entusiasmado em aplicar meu conhecimento e habilidades em projetos desafiadores e inovadores. 
+    Sou desenvolvedora frontend com um forte conjunto de habilidades em tecnologias como HTML, CSS, JavaScript, Typescript, e React. Tenho experiência em criar interfaces responsivas e atraentes utilizando frameworks e bibliotecas como Bootstrap, Sass, e Material-UI (MUI). Além disso, possuo conhecimentos em PHP, Node.js, MySQL, e APIs. Durante minha jornada acadêmica e experiência profissional, participei de diversos projetos web, desde a concepção até a aplicação de designs criados no Figma, garantindo fidelidade e funcionalidade nas entregas. Também tenho experiência com ferramentas de design como Photoshop, e sou adepta do uso de Wordpress (Elementor) para criação de sites dinâmicos. Atualmente, estou no último período da faculdade e estou pronta para transformar ideias inovadoras em soluções práticas e funcionais que realmente façam a diferença. Meu objetivo é criar experiências de usuário excepcionais que impulsionem o sucesso dos projetos.
                 </p>
+
                 <p className="about-text">
-                Meu foco é sempre entregar soluções de alta qualidade e contribuir para o sucesso das equipes em que trabalho.
-                </p>
+    Meu foco é sempre entregar soluções de alta qualidade que não apenas atendam, mas superem as expectativas dos clientes e usuários. Gosto de trabalhar de maneira colaborativa, valorizando a comunicação e a troca de ideias dentro da equipe. Sou detalhista e comprometida com o aprimoramento contínuo, sempre buscando aprender novas técnicas e melhores práticas em desenvolvimento frontend. Durante minha experiência profissional, aprendi a lidar com prazos apertados e a entregar projetos dentro do prazo. Estou sempre pronta para enfrentar novos desafios e contribuir para o sucesso dos projetos em que estou envolvida, mantendo-me atualizada com as tendências e avanços tecnológicos.
+              </p>
+
             </div>
             <div className='about-icon-button'>
               <div className="about-img">  
                 <IconsTec />
               </div>
-              {/* <div className='about-btn'>
-                <Button className="sec">Baixar cv</Button>
-              </div> */}
+              <div className='about-btn'>
+                <CustomButton>Baixar CV</CustomButton> 
+              </div>
+        
             </div>
         </div>
     </div>
