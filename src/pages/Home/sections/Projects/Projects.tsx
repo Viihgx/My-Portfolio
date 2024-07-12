c8ad5d812c7a01d8a7de9f9860ed4a77f2d06325
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-12 13:01:01 -0300

    utiliza component custombutton, ajusta css

--- src/pages/Home/sections/Projects/Projects.tsx
+++ src/pages/Home/sections/Projects/Projects.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import './Projects.css';
 import Project1 from '../../../../assets/images/ImgProject1.png';
 import ButtonTec from '../../../../components/ButtonTec/ButtonTec';
-import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare, FaBootstrap, FaFigma } from 'react-icons/fa';
+import CustomButton from '../../../../components/CustomButton/CustomButton';
+import { FaHtml5, FaCss3Alt, FaReact, FaJsSquare, FaBootstrap, FaFigma, FaGithub } from 'react-icons/fa';
 
 const projects = [
   {
     title: 'Suco Puro',
-    description: 'Desenvolvi uma loja virtual de sucos naturais, focada em proporcionar uma experiência de usuário agradável e responsiva. A aplicação inclui uma homepage interativa com animações, exibição de produtos, uma seção de benefícios dos produtos, informações de contato e um carrinho de compras.',
+    description: 'Loja virtual de sucos naturais, desenvolvida com foco na experiência do usuário, responsividade, animações suaves e design moderno. A loja oferece funcionalidades de compra e navegação intuitiva entre as seções. A aplicação inclui uma homepage, uma seção "Sobre Nós", informações sobre os benefícios dos produtos, exibição dos produtos para compra, uma seção de contato com informações detalhadas, e um carrinho de compras funcional. ',
+    description2: 'Tecnologias utilizadas: React, Bootstrap, Framer Motion, React Icons, JavaScript (ES6+), CSS, HTML',
     images: {
       pc: Project1,
     },
     url: 'https://sucopuro.vercel.app/',
+    github: 'https://github.com/Viihgx/juice-store',
     techs: [
       { icon: <FaReact />, label: 'React' },
       { icon: <FaBootstrap />, label: 'Bootstrap' },
@@ -51,13 +54,14 @@ const projects = [
 const Projects = () => {
   return (
     <div className="projects-container" id="projects">
-      <h1 className="project-title">Meus Projetos</h1>
+      <h1 className="container-project-title">Meus Projetos</h1>
       <div className="projects-content">
         {projects.map((project, index) => (
           <div className={`project-item ${index % 2 === 0 ? 'text-left' : 'text-right'}`} key={index}>
             <div className="project-description">
               <h2 className='project-title'>{project.title}</h2>
               <p className='project-text'>{project.description}</p>
+              <p className='project-text-description2'>{project.description2}</p>
               {project.techs && (
                 <div className="tech-icons">
                   {project.techs.map((tech, index) => (
@@ -65,6 +69,14 @@ const Projects = () => {
                   ))}
                 </div>
               )}
+              <div className="project-buttons">
+                <CustomButton className="variant-secondary" onClick={() => window.open(project.github, '_blank')}>
+                  <FaGithub /> GitHub
+                </CustomButton>
+                <CustomButton className="variant-secondary" onClick={() => window.open(project.url, '_blank')}>
+                  Acessar Página
+                </CustomButton>
+              </div>
             </div>
             <div className="project-image">
               <div className="browser-frame">
