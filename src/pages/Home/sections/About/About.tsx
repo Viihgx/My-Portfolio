954f0ecfd54872319f6aa3745fe0932ce09f697f
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-15 09:42:38 -0300

    :lipstick: Adiciona responsividade no about e custombutton

--- src/pages/Home/sections/About/About.tsx
+++ src/pages/Home/sections/About/About.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './About.css';
-import Carousel from '../../../../components/Carousel/Carousel';
 import IconsTec from '../../../../components/IconTec/IconsTec';
 import CustomButton from '../../../../components/CustomButton/CustomButton';
 
