861a65598250045a3cb49fd5ed4476fbd5f9ea55
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-15 09:41:48 -0300

    :lipstick: Muda nome do site, ajusta css global

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.tsx'
 import './index.css'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import theme from './theme.ts'
