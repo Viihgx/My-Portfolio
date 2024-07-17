4134f29756d02edc969ca2fc5f1998af0de61548
Viihgx <vitoriasamara.g77@gmail.com>
2024-07-17 00:31:55 -0300

    :sparkles: Ajusta build...

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ParallaxContainer from './pages/Parallax/ParallaxContainer';
-import React from 'react';
 // import './index.css';
 
 const App = () => {
